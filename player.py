6a3822ea390ecf2271b89d499423f5389f4953f8
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-11-11 21:11:18 -0600

    movement done

---
+++ player.py
@@ -0,0 +1,63 @@
+from settings import *
+import pygame as pg
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pg.key.get_pressed()
+        speed = PLAYER_SPEED * self.game.delta_time
+
+        dx, dy = 0, 0
+        if keys[pg.K_w]:
+            dx += speed * cos_a
+            dy += speed * sin_a
+        if keys[pg.K_s]:
+            dx -= speed * cos_a
+            dy -= speed * sin_a
+        if keys[pg.K_a]:
+            dx += speed * sin_a
+            dy -= speed * cos_a
+        if keys[pg.K_d]:
+            dx -= speed * sin_a
+            dy += speed * cos_a
+
+        self.x += dx
+        self.y += dy
+
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau
+
+    def draw(self):
+        pg.draw.line(
+            self.game.screen,
+            "yellow",
+            (self.x * 100, self.y * 100),
+            (
+                self.x * 100 + WIDTH * math.cos(self.angle),
+                self.y * 100 + WIDTH * math.sin(self.angle),
+            ),
+            2,
+        )
+        pg.draw.circle(self.game.screen, "green", (self.x * 100, self.y * 100), 15)
+
+    def update(self):
+        self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
